dad6baa5b7e66c0f78922e14f115977f988f0ba6
Dmitry Brant <dbrant@wikimedia.org>
2015-10-25 00:26:26 -0400

    Added planets, and zoom-on-hover.

--- app/src/main/java/org/gearvrf/planetarium/PlanetariumViewManager.java
+++ app/src/main/java/org/gearvrf/planetarium/PlanetariumViewManager.java
@@ -20,6 +20,12 @@ import android.opengl.Matrix;
 import android.os.Environment;
 import android.view.Gravity;
 
+import com.mhuss.AstroLib.AstroDate;
+import com.mhuss.AstroLib.NoInitException;
+import com.mhuss.AstroLib.ObsInfo;
+import com.mhuss.AstroLib.PlanetData;
+import com.mhuss.AstroLib.Planets;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -30,6 +36,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Future;
 
@@ -51,6 +58,7 @@ import org.gearvrf.animation.GVRAnimation;
 import org.gearvrf.animation.GVRAnimationEngine;
 import org.gearvrf.animation.GVRRepeatMode;
 import org.gearvrf.animation.GVRRotationByAxisWithPivotAnimation;
+import org.gearvrf.animation.GVRScaleAnimation;
 import org.gearvrf.scene_objects.GVRCubeSceneObject;
 import org.gearvrf.scene_objects.GVRSphereSceneObject;
 import org.gearvrf.scene_objects.GVRTextViewSceneObject;
@@ -68,6 +76,7 @@ public class PlanetariumViewManager extends GVRScript {
     private GVRScene mMainScene;
 
     private List<Star> starList;
+    private List<GVRAnimation> unzoomAnimationList = new ArrayList<>();
 
     GVRTextViewSceneObject textView;
 
@@ -183,8 +192,8 @@ public class PlanetariumViewManager extends GVRScript {
         mMainScene.addSceneObject(solarSystemObject);
 
         GVRMesh mesh = gvrContext.createQuad(10f, 10f);
-        Future<GVRMesh> futureMesh = new FutureWrapper<>(mesh);
-        GVRTexture gtex = new GVRBitmapTexture(gvrContext, "star2.png");
+        //Future<GVRMesh> futureMesh = new FutureWrapper<>(mesh);
+        //GVRTexture gtex = new GVRBitmapTexture(gvrContext, "star2.png");
         Future<GVRTexture> futureTex = gvrContext.loadFutureTexture(new GVRAndroidResource(gvrContext, R.drawable.star2));
 
         GVRMaterial gmat = new GVRMaterial(gvrContext);
@@ -199,124 +208,41 @@ public class PlanetariumViewManager extends GVRScript {
             GVRSceneObject sobj = new GVRSceneObject(gvrContext, mesh);
             sobj.getRenderData().setMaterial(gmat);
             sobj.getRenderData().setDepthTest(false);
-            sobj.getTransform().setPosition(0, 0, -star.dist);
             float scale = 1 / (star.mag < 0.5f ? 0.5f : star.mag);
             sobj.getTransform().setScale(scale, scale, scale);
-            sobj.getTransform().rotateByAxisWithPivot((float) star.ra, 0, 1, 0, 0, 0, 0);
-
-            float x1 = 1, z1 = 0;
-            float c = (float) Math.cos(Math.toRadians(star.ra));
-            float s = (float) Math.sin(Math.toRadians(star.ra));
-            float xnew = x1 * c - z1 * s;
-            float znew = x1 * s + z1 * c;
-            sobj.getTransform().rotateByAxisWithPivot((float) -star.dec, xnew, 0, znew, 0, 0, 0);
-
+            setObjectPosition(sobj, star.ra, star.dec, star.dist);
             sobj.setName(star.name == null ? "" : star.name);
             sobj.attachEyePointeeHolder();
 
             mMainScene.addSceneObject(sobj);
         }
 
+        try {
+            Date now = new Date();
 
+            // TODO: fix
+            //AstroDate date = new AstroDate(now.getDay(), now.getMonth(), now.getYear());
+            Log.d(TAG, ">>>>>>>>>>>>> year: " + now.getYear());
+            Log.d(TAG, ">>>>>>>>>>>>> month: " + now.getMonth());
+            Log.d(TAG, ">>>>>>>>>>>>> day: " + now.getYear());
 
+            AstroDate date = new AstroDate(24, 10, 2015);
+            ObsInfo obsInfo = new ObsInfo();
+            double jd = date.jd();
 
-        GVRSceneObject earthRevolutionObject = new GVRSceneObject(gvrContext);
-        earthRevolutionObject.getTransform().setPosition(20.0f, 0.0f, -20.0f);
-        solarSystemObject.addChildObject(earthRevolutionObject);
-
-        GVRSceneObject earthRotationObject = new GVRSceneObject(gvrContext);
-        earthRevolutionObject.addChildObject(earthRotationObject);
-
-        GVRSceneObject earthMeshObject = asyncSceneObject(gvrContext, "earthmap1k.jpg");
-        earthMeshObject.getTransform().setScale(1.0f, 1.0f, 1.0f);
-        earthRotationObject.addChildObject(earthMeshObject);
-
-        counterClockwise(earthRotationObject, 1.5f);
-
-
-/*
-
-        GVRSceneObject sunRotationObject = new GVRSceneObject(gvrContext);
-        solarSystemObject.addChildObject(sunRotationObject);
-
-        GVRSceneObject sunMeshObject = asyncSceneObject(gvrContext, "sunmap.astc");
-        sunMeshObject.getTransform().setPosition(0.0f, 0.0f, 0.0f);
-        sunMeshObject.getTransform().setScale(10.0f, 10.0f, 10.0f);
-        sunRotationObject.addChildObject(sunMeshObject);
-
-
-        GVRSceneObject mercuryRevolutionObject = new GVRSceneObject(gvrContext);
-        mercuryRevolutionObject.getTransform().setPosition(14.0f, 0.0f, 0.0f);
-        solarSystemObject.addChildObject(mercuryRevolutionObject);
-
-        GVRSceneObject mercuryRotationObject = new GVRSceneObject(gvrContext);
-        mercuryRevolutionObject.addChildObject(mercuryRotationObject);
-
-        GVRSceneObject mercuryMeshObject = asyncSceneObject(gvrContext,
-                "mercurymap.jpg");
-        mercuryMeshObject.getTransform().setScale(0.3f, 0.3f, 0.3f);
-        mercuryRotationObject.addChildObject(mercuryMeshObject);
-
-        GVRSceneObject venusRevolutionObject = new GVRSceneObject(gvrContext);
-        venusRevolutionObject.getTransform().setPosition(17.0f, 0.0f, 0.0f);
-        solarSystemObject.addChildObject(venusRevolutionObject);
-
-        GVRSceneObject venusRotationObject = new GVRSceneObject(gvrContext);
-        venusRevolutionObject.addChildObject(venusRotationObject);
-
-        GVRSceneObject venusMeshObject = asyncSceneObject(gvrContext,
-                "venusmap.jpg");
-        venusMeshObject.getTransform().setScale(0.8f, 0.8f, 0.8f);
-        venusRotationObject.addChildObject(venusMeshObject);
-
-        GVRSceneObject earthRevolutionObject = new GVRSceneObject(gvrContext);
-        earthRevolutionObject.getTransform().setPosition(22.0f, 0.0f, 0.0f);
-        solarSystemObject.addChildObject(earthRevolutionObject);
-
-        GVRSceneObject earthRotationObject = new GVRSceneObject(gvrContext);
-        earthRevolutionObject.addChildObject(earthRotationObject);
-
-        GVRSceneObject earthMeshObject = asyncSceneObject(gvrContext,
-                "earthmap1k.jpg");
-        earthMeshObject.getTransform().setScale(1.0f, 1.0f, 1.0f);
-        earthRotationObject.addChildObject(earthMeshObject);
-
-        GVRSceneObject moonRevolutionObject = new GVRSceneObject(gvrContext);
-        moonRevolutionObject.getTransform().setPosition(4.0f, 0.0f, 0.0f);
-        earthRevolutionObject.addChildObject(moonRevolutionObject);
-        moonRevolutionObject.addChildObject(mMainScene.getMainCameraRig());
-
-        GVRSceneObject marsRevolutionObject = new GVRSceneObject(gvrContext);
-        marsRevolutionObject.getTransform().setPosition(30.0f, 0.0f, 0.0f);
-        solarSystemObject.addChildObject(marsRevolutionObject);
-
-        GVRSceneObject marsRotationObject = new GVRSceneObject(gvrContext);
-        marsRevolutionObject.addChildObject(marsRotationObject);
-
-        GVRSceneObject marsMeshObject = asyncSceneObject(gvrContext,
-                "mars_1k_color.jpg");
-        marsMeshObject.getTransform().setScale(0.6f, 0.6f, 0.6f);
-        marsRotationObject.addChildObject(marsMeshObject);
-
-        counterClockwise(sunRotationObject, 50f);
-
-        counterClockwise(mercuryRevolutionObject, 150f);
-        counterClockwise(mercuryRotationObject, 100f);
-
-        counterClockwise(venusRevolutionObject, 400f);
-        clockwise(venusRotationObject, 400f);
-
-        counterClockwise(earthRevolutionObject, 600f);
-        counterClockwise(earthRotationObject, 1.5f);
-
-        counterClockwise(moonRevolutionObject, 60f);
-
-        //clockwise(mMainScene.getMainCameraRig().getTransform(), 60f);
-
-        counterClockwise(marsRevolutionObject, 1200f);
-        counterClockwise(marsRotationObject, 200f);
-        */
-
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.SUN, "gstar.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.LUNA, "moon.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.MERCURY, "mercurymap.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.VENUS, "venusmap.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.MARS, "mars_1k_color.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.JUPITER, "jupiter.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.SATURN, "saturn.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.URANUS, "uranus.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.NEPTUNE, "neptune.jpg");
+            addPlanetObject(gvrContext, solarSystemObject, jd, obsInfo, Planets.PLUTO, "pluto.jpg");
+        }catch(Exception e) {
+            //
+        }
     }
 
     @Override
@@ -325,6 +251,20 @@ public class PlanetariumViewManager extends GVRScript {
         for (GVRPicker.GVRPickedObject pickedObject : GVRPicker.findObjects(mContext.getMainScene())) {
             //mMainScene.addStatMessage("Picked: " + pickedObject.getHitObject().getName());
             textView.setText(pickedObject.getHitObject().getName());
+
+
+            if (unzoomAnimationList.size() > 0) {
+                for (GVRAnimation anim : unzoomAnimationList) {
+                    anim.start(mAnimationEngine);
+                }
+                unzoomAnimationList.clear();
+            }
+
+            GVRScaleAnimation anim = new GVRScaleAnimation(pickedObject.getHitObject(), 0.3f, 4.0f);
+            anim.start(mAnimationEngine);
+
+            GVRScaleAnimation unanim = new GVRScaleAnimation(pickedObject.getHitObject(), 0.3f, 0.7f);
+            unzoomAnimationList.add(unanim);
         }
 
     }
@@ -336,6 +276,38 @@ public class PlanetariumViewManager extends GVRScript {
             mMainScene.setStatsEnabled(!statsEnabled);
         }
     }
+
+
+    private void setObjectPosition(GVRSceneObject obj, double ra, double dec, float dist) {
+        obj.getTransform().setPosition(0, 0, -dist);
+        obj.getTransform().rotateByAxisWithPivot((float) ra, 0, 1, 0, 0, 0, 0);
+        float x1 = 1, z1 = 0;
+        float c = (float) Math.cos(Math.toRadians(ra));
+        float s = (float) Math.sin(Math.toRadians(ra));
+        float xnew = x1 * c - z1 * s;
+        float znew = x1 * s + z1 * c;
+        obj.getTransform().rotateByAxisWithPivot((float) -dec, xnew, 0, znew, 0, 0, 0);
+    }
+
+    private void addPlanetObject(GVRContext context, GVRSceneObject parentObj, double julianDate,
+                                 ObsInfo obsInfo, int planetID,
+                                 String planetTexture) throws IOException, NoInitException {
+        PlanetData data = new PlanetData(planetID, julianDate, obsInfo);
+        GVRSceneObject planetRevolutionObject = new GVRSceneObject(context);
+        setObjectPosition(planetRevolutionObject, Math.toDegrees(data.getRightAscension()), Math.toDegrees(data.getDeclination()), 50);
+        parentObj.addChildObject(planetRevolutionObject);
+
+        GVRSceneObject planetRotationObject = new GVRSceneObject(context);
+        planetRevolutionObject.addChildObject(planetRotationObject);
+        GVRSceneObject planetMeshObject = asyncSceneObject(context, planetTexture);
+
+        planetMeshObject.getTransform().setScale(1.0f, 1.0f, 1.0f);
+        planetRotationObject.addChildObject(planetMeshObject);
+        counterClockwise(planetRotationObject, 10f);
+        planetMeshObject.attachEyePointeeHolder();
+        planetMeshObject.setName(planetTexture);
+    }
+
 
     private List<GVRAnimation> mAnimations = new ArrayList<>();
 
